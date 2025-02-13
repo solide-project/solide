9421a07dcbf76f48ed66be3a07752b5bfcae9cc6
5208980 <z5208980@zmail.unsw.edu.au>
2025-02-13 22:32:44 +1100

    fix name

--- lib/chains/name.ts
+++ lib/chains/name.ts
@@ -177,7 +177,7 @@ export const data: { [key: string]: string } = {
   [ChainID.STACK]: "Stack",
   [ChainID.STACK_TESTNET]: "Stack Testnet",
   [ChainID.HEMI_TESTNET]: "Hemi Sepolia",
-  [ChainID.MINT]: "Hemi Mainnet",
+  [ChainID.MINT]: "Mint Mainnet",
   [ChainID.MINT_SEPOLIA]: "Mint Sepolia",
   [ChainID.TABI_TESTNET]: "Tabi Testnet",
   [ChainID.SUPERPOSITION]: "Superposition",
